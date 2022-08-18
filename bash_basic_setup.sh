0ce84b6572ea17cf0414fa74912980ae27cedb82
Ryan Dalby <dalbyryan3@gmail.com>
2022-08-18 13:50:17 -0600

    Update bash basic setup script

--- bash_basic_setup.sh
+++ bash_basic_setup.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get install curl -y
+sudo apt-get install wget -y
 sudo apt-get install vim -y
 sudo apt-get install tmux -y
 sudo apt-get install git -y
-sudo apt-get install fzf -y
 sudo apt-get install silversearcher-ag -y
 
 git clone https://github.com/dalbyryan3/dot-files.git ~/dot-files
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-ln -s ~/dot-files/.vimrc ~/.vimrc 
+ln -s ~/dot-files/.vimrc ~/.vimrc
 ln -s ~/dot-files/.tmux.conf ~/.tmux.conf
 ln -s ~/dot-files/.gitconfig ~/.gitconfig
 ln -s ~/dot-files/.gitignore ~/.gitignore
 ln -s ~/dot-files/.gitattributes ~/.gitattributes
 
-echo "export VISUAL=vim" >> ~/.bashrc
-echo "export EDITOR=\"$VISUAL\"" >> ~/.bashrc
-echo "alias tmux=\"tmux -2\"" >> ~/.bashrc
-echo "alias ll=\"ls -latr\"" >> ~/.bashrc
-echo "alias gs=\"git status\"" >> ~/.bashrc
-echo "alias gl=\"git log\"" >> ~/.bashrc
-echo "alias ga=\"git add\"" >> ~/.bashrc
-echo "alias gc=\"git commit\"" >> ~/.bashrc
-echo "alias gd=\"git diff\"" >> ~/.bashrc
+vim -E -c PluginInstall -c qa
 
-vim -E -c PluginInstall -c q
-source ~/.tmux/plugins/tpm/scripts/install_plugins.sh
-source ~/.bashrc
+yes | source ~/.vim/bundle/fzf/install
+yes | source ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+grep -qxF 'export VISUAL=vim' ~/.bashrc || echo 'export VISUAL=vim' >> ~/.bashrc
+grep -qxF 'export EDITOR="$VISUAL"' ~/.bashrc || echo 'export EDITOR="$VISUAL"' >> ~/.bashrc
+grep -qxF 'alias tmux="tmux -2"' ~/.bashrc || echo 'alias tmux="tmux -2"' >> ~/.bashrc
+grep -qxF 'alias ll="ls -latr"' ~/.bashrc || echo 'alias ll="ls -latr"' >> ~/.bashrc
+grep -qxF 'alias gs="git status"' ~/.bashrc || echo 'alias gs="git status"' >> ~/.bashrc
+grep -qxF 'alias gl="git log"' ~/.bashrc || echo 'alias gl="git log"' >> ~/.bashrc
+grep -qxF 'alias ga="git add"' ~/.bashrc || echo 'alias ga="git add"' >> ~/.bashrc
+grep -qxF 'alias gc="git commit"' ~/.bashrc || echo 'alias gc="git commit"' >> ~/.bashrc
+grep -qxF 'alias gd="git diff"' ~/.bashrc || echo 'alias gd="git diff"' >> ~/.bashrc
+
+echo
+echo -e "\033[1mShould source ~/.bashrc or restart shell now"
+echo
