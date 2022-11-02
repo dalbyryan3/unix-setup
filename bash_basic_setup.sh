4d95426e125a6a97416a28dcaeba49f6607582f3
Ryan Dalby <dalbyryan3@gmail.com>
2022-11-02 14:03:30 -0600

    Update bash_basic_setup.sh

--- bash_basic_setup.sh
+++ bash_basic_setup.sh
@@ -33,6 +33,7 @@ grep -qxF 'alias gl="git log"' ~/.bashrc || echo 'alias gl="git log"' >> ~/.bash
 grep -qxF 'alias ga="git add"' ~/.bashrc || echo 'alias ga="git add"' >> ~/.bashrc
 grep -qxF 'alias gc="git commit"' ~/.bashrc || echo 'alias gc="git commit"' >> ~/.bashrc
 grep -qxF 'alias gcm="git commit -m"' ~/.bashrc || echo 'alias gcm="git commit -m"' >> ~/.bashrc
+grep -qxF 'alias gcam="git commit -am"' ~/.bashrc || echo 'alias gcam="git commit -am"' >> ~/.bashrc
 grep -qxF 'alias gd="git diff"' ~/.bashrc || echo 'alias gd="git diff"' >> ~/.bashrc
 
 echo
